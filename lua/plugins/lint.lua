fc5bb006977c1bdf5e5a6d8e0317914171281c7a
winoooops <winoooops@outlook.com>
2026-04-10 09:48:56 -0600

    feat(plugins): add nvim-lint for linting (replaces null-ls)

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -1,1 +1,22 @@
-return {}
+-- lua/plugins/lint.lua
+-- nvim-lint replaces null-ls for linting.
+
+return {
+  "mfussenegger/nvim-lint",
+  event = { "BufReadPost", "BufNewFile", "BufWritePost" },
+  config = function()
+    local lint = require("lint")
+    lint.linters_by_ft = {
+      python = { "ruff" },
+      javascript = { "eslint_d" },
+      typescript = { "eslint_d" },
+      typescriptreact = { "eslint_d" },
+      javascriptreact = { "eslint_d" },
+      sh = { "shellcheck" },
+      markdown = { "markdownlint" },
+    }
+    vim.api.nvim_create_autocmd({ "BufReadPost", "BufWritePost", "InsertLeave" }, {
+      callback = function() lint.try_lint() end,
+    })
+  end,
+}
